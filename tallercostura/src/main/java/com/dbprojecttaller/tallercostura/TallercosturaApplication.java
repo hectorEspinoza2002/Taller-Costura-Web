cace8385546bd5bde1f98c812edd8e4c2317c822
Héctor Espinoza <hectorfrancisco1214@gmail.com>
2025-12-09 18:57:57 -0600

    first commit

---
+++ tallercostura/src/main/java/com/dbprojecttaller/tallercostura/TallercosturaApplication.java
@@ -0,0 +1,13 @@
+package com.dbprojecttaller.tallercostura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallercosturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallercosturaApplication.class, args);
+	}
+
+}
---
+++ tallercostura/src/test/java/com/dbprojecttaller/tallercostura/TallercosturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbprojecttaller.tallercostura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallercosturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
