94d001d9c50ded485a9ad8321dd51afb39d2b449
Héctor Espinoza <hectorfrancisco1214@gmail.com>
2025-12-29 20:58:22 -0600

    bandera

--- tallercostura/src/main/java/com/dbprojecttaller/tallercostura/TallercosturaApplication.java
+++ tallercostura/src/main/java/com/dbprojecttaller/tallercostura/TallercosturaApplication.java
@@ -8,6 +8,9 @@ public class TallercosturaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TallercosturaApplication.class, args);
+
+		System.out.println("< ===== Funciona! ===== >");
+
 	}
 
 }
